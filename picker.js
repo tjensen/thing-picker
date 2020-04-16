4fb24dcbd9a5081258ed00d6db66d00ef25b2995
Tim Jensen <tim.l.jensen@gmail.com>
2020-04-15 21:01:13 -0500

    Adding Javascript for doing the random picking and stuff

---
+++ picker.js
@@ -0,0 +1,85 @@
+(function () {
+  let share;
+
+  if (window.location.search.match(/^\?/)) {
+    const params = window.location.search.substr(1).split('&');
+    params.forEach(function(param) {
+      const parts = param.split('=');
+      if (decodeURIComponent(parts[0]) === 'share') {
+        share = atob(decodeURIComponent(parts[1]));
+      }
+    });
+  }
+
+  const choices = document.getElementById('choices');
+  let numChoices = 0;
+
+  function addChoice(text) {
+    const input = document.createElement('input');
+    input.setAttribute('id', 'thing-' + numChoices);
+    input.setAttribute('type', 'text');
+    if (text) {
+      input.setAttribute('value', text);
+    }
+    input.setAttribute('placeholder', 'Thing ' + (numChoices + 1));
+    if (share) {
+      input.setAttribute('readonly', 'true');
+    }
+    choices.appendChild(input);
+    numChoices += 1;
+  }
+
+  if (share) {
+    share.split('\0').forEach(function(choice) {
+      addChoice(choice);
+    });
+  }
+  else {
+    addChoice();
+    addChoice();
+  }
+
+  function onPick() {
+    const pick = Math.floor(Math.random() * Math.floor(numChoices));
+
+    const yourPick = document.getElementById('your-pick');
+    yourPick.innerHTML = 'Your pick is:';
+
+    const pickText = document.createElement('div');
+    pickText.setAttribute('id', 'your-pick-text');
+    pickText.innerText = document.getElementById('thing-' + pick).value;
+    yourPick.appendChild(pickText);
+  }
+
+  function onAddAnother() {
+    addChoice();
+  }
+
+  function onShare() {
+    const choiceTexts = [];
+    for (let i = 0; i < numChoices; i++) {
+      choiceTexts.push(document.getElementById('thing-' + i).value);
+    }
+    const code = btoa(choiceTexts.join('\0')).replace(/=+$/, '');
+    window.location.search = '?share=' + code;
+  }
+
+  const buttons = document.getElementById('buttons');
+
+  const pickButton = document.createElement('button');
+  pickButton.innerText = 'Pick';
+  pickButton.addEventListener('click', onPick);
+  buttons.appendChild(pickButton);
+
+  if (!share) {
+    const addAnotherButton = document.createElement('button');
+    addAnotherButton.innerText = 'Add Another Thing';
+    addAnotherButton.addEventListener('click', onAddAnother);
+    buttons.appendChild(addAnotherButton);
+
+    const shareButton = document.createElement('button');
+    shareButton.innerText = 'Share';
+    shareButton.addEventListener('click', onShare);
+    buttons.appendChild(shareButton);
+  }
+})();
